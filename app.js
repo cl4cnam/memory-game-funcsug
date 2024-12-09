52eca7d5f2e5b13a4794ff8c5bc7aa2c99ee5c82
cli345 <cli345@example.net>
2024-12-09 21:53:53 +0100

    version FuncSug

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
+    function init() {
+
     const cardArray = [
         { name: "blue_square", img: "images/blue_square.png" },
         { name: "blue_square", img: "images/blue_square.png" },
@@ -14,17 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
         { name: "yellow_triangle", img: "images/yellow_triangle.png" }
     ];
 
-    cardArray.sort(() => Math.random() - 0.5);
+    return cardArray;
 
-    const grid = document.querySelector(".grid");
-    const resultDisplay = document.querySelector("#result");
-    const restartButton = document.querySelector("#restart");
-    let cardsChosen = [];
-    let cardsChosenId = [];
-    let cardsWon = [];
-    let lockBoard = false;
+    }
 
-    function createBoard() {
+    function createBoard(cardArray) {
+        const grid = document.querySelector(".grid");
         grid.innerHTML = "";
         for (let i = 0; i < cardArray.length; i++) {
             const card = document.createElement("div");
@@ -50,65 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             card.appendChild(cardInner);
 
-            card.addEventListener("click", flipCard);
             grid.appendChild(card);
         }
-        resultDisplay.textContent = 0;
     }
 
-    function checkForMatch() {
-        const cards = document.querySelectorAll(".card");
-        const [optionOneId, optionTwoId] = cardsChosenId;
-
-        if (optionOneId === optionTwoId) {
-            cards[optionOneId].classList.remove("flipped");
-        } else if (cardsChosen[0] === cardsChosen[1]) {
-            cards[optionOneId].removeEventListener("click", flipCard);
-            cards[optionTwoId].removeEventListener("click", flipCard);
-            cardsWon.push(cardsChosen);
-        } else {
-            setTimeout(() => {
-                cards[optionOneId].classList.remove("flipped");
-                cards[optionTwoId].classList.remove("flipped");
-            }, 100);
-        }
-
-        cardsChosen = [];
-        cardsChosenId = [];
-
-        lockBoard = false;
-
-        resultDisplay.textContent = cardsWon.length;
-
-        if (cardsWon.length === cardArray.length / 2) {
-            resultDisplay.textContent = "Congratulations! You Won!";
-        }
-    }
-
-    function flipCard() {
-        if (lockBoard) return;
-        
-        const cardId = this.getAttribute("data-id");
-        if (!cardsChosenId.includes(cardId)) {
-            cardsChosen.push(cardArray[cardId].name);
-            cardsChosenId.push(cardId);
-            this.classList.add("flipped");
-
-            if (cardsChosen.length === 2) {
-                lockBoard = true;
-                setTimeout(checkForMatch, 500);
-            }
-        }
-    }
-
-    restartButton.addEventListener("click", () => {
-        cardsChosen = [];
-        cardsChosenId = [];
-        cardsWon = [];
-        cardArray.sort(() => Math.random() - 0.5);
-        lockBoard = false;
-        createBoard();
-    });
-
-    createBoard();
-});
